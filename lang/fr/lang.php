00714cbbfab2ef1f1fb0f57c19b2131872927547
Johann Saunier <jsaunier@aprogsys.com>
2010-05-13 18:42:01 +0200

    French translation added

---
+++ lang/fr/lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['js']['up']    = 'Mot précédent';
+$lang['js']['down']  = 'Mot suivant';
+$lang['js']['close'] = 'Fermer et supprimer la mise en surbriance des mots trouvés';
+$lang['js']['start'] = 'Fin du document atteinte, reprendre au début';
+$lang['js']['end']   = 'Début du document atteint, reprendre à la fin';
