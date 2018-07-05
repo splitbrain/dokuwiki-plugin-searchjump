2d81735164ffdf3aa6ca56db50d942d205bc090a
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:40:35 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['js']['up']              = 'Destaques de pesquisa anterior';
+$lang['js']['down']            = 'Próxima pesquisa';
+$lang['js']['close']           = 'Fechar e remover o destaque de resultados da pesquisa';
+$lang['js']['start']           = 'Fim do documento alcançado, continuando no topo.';
+$lang['js']['end']             = 'Início do documento alcançado, continuando no final.';
