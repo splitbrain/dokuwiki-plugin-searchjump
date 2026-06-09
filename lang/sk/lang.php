49442d19cd59b1c0227a7781ea56f2a3ecc8e23c
Wizzard <wizzardsk@gmail.com>
2026-06-09 22:20:50 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['js']['up']              = 'Predchádzajúci výskyt';
+$lang['js']['down']            = 'Ďalší výskyt';
+$lang['js']['close']           = 'Zavrieť a odstrániť zvýraznenie výskytov';
+$lang['js']['start']           = 'Dosiahnutý koniec dokumentu, pokračuje sa od začiatku.';
+$lang['js']['end']             = 'Dosiahnutý začiatok dokumentu, pokračuje sa od konca.';
